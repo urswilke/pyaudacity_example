ce426f74e2dc59fa936333c27d34d3edb5275e62
urswilke <urs.wilke@gmail.com>
2023-03-15 11:41:50 +0100

    put input file name in the beginning of the script

--- sultans_of_swing_modify.py
+++ sultans_of_swing_modify.py
@@ -1,8 +1,13 @@
 # start audacity from within the folder of this script before!
 import pyaudacity as pa
 
+# put the name of your audio file in this folder:
+input_mp3 = "sultans_of_swing.mp3"
+basename = input_mp3.replace(".mp3", "")
+
+
 for s in ["Hard Clipping", "Soft Clipping", "Soft Overdrive", "Medium Overdrive", "Hard Overdrive", "Cubic Curve (odd harmonics)", "Even Harmonics", "Expand and Compress", "Leveller", "Rectifier Distortion", "Hard Limiter 1413"]:
-    pa.do('Import2: Filename="sultans_of_swing.mp3"')
+    pa.do('Import2: Filename="' + input_mp3 + '"')
     pa.do('SelectTime: Start="54.5" End="100"')
     pa.do('Delete')
     pa.do('SelectTime: Start="0" End="2.6"')
@@ -13,5 +18,5 @@ for s in ["Hard Clipping", "Soft Clipping", "Soft Overdrive", "Medium Overdrive"
     pa.do('Normalize')
     pa.do('Compressor: Threshold=-20')
     pa.do('Distortion: Threshold=-40 Type="' + s + '"')
-    pa.do('Export2: Filename="sultans_of_swing_pyaudacity_' + s + '.mp3"')
+    pa.do('Export2: Filename="' + basename + '_pyaudacity_' + s + '.mp3"')
     pa.do('TrackClose')
