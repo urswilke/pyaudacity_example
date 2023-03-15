d4d8a387e7f018c23535117a91fc6c34902e3ffb
urswilke <urs.wilke@gmail.com>
2023-03-15 11:15:43 +0100

    start audacity from within this folder and use relative paths

--- sultans_of_swing_modify.py
+++ sultans_of_swing_modify.py
@@ -1,7 +1,8 @@
+# start audacity from within the folder of this script before!
 import pyaudacity as pa
 
 for s in ["Hard Clipping", "Soft Clipping", "Soft Overdrive", "Medium Overdrive", "Hard Overdrive", "Cubic Curve (odd harmonics)", "Even Harmonics", "Expand and Compress", "Leveller", "Rectifier Distortion", "Hard Limiter 1413"]:
-    pa.do('Import2: Filename="/home/gspusi/Music/aufnahmen/sultans_of_swing.mp3"')
+    pa.do('Import2: Filename="sultans_of_swing.mp3"')
     pa.do('SelectTime: Start="54.5" End="100"')
     pa.do('Delete')
     pa.do('SelectTime: Start="0" End="2.6"')
@@ -12,5 +13,5 @@ for s in ["Hard Clipping", "Soft Clipping", "Soft Overdrive", "Medium Overdrive"
     pa.do('Normalize')
     pa.do('Compressor: Threshold=-20')
     pa.do('Distortion: Threshold=-40 Type="' + s + '"')
-    pa.do('Export2: Filename="/home/gspusi/Music/aufnahmen/sultans_of_swing_pyaudacity_' + s + '.mp3"')
+    pa.do('Export2: Filename="sultans_of_swing_pyaudacity_' + s + '.mp3"')
     pa.do('TrackClose')
