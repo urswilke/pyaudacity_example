5dc62ee8daeb4271cf4aa25bb6530e87d06993c0
urswilke <urs.wilke@gmail.com>
2023-03-15 11:14:18 +0100

    use different distortion types

--- sultans_of_swing_modify.py
+++ sultans_of_swing_modify.py
@@ -1,6 +1,6 @@
 import pyaudacity as pa
-for i in [5, 10, 20, 40, 80]:
-    t = str(i)
+
+for s in ["Hard Clipping", "Soft Clipping", "Soft Overdrive", "Medium Overdrive", "Hard Overdrive", "Cubic Curve (odd harmonics)", "Even Harmonics", "Expand and Compress", "Leveller", "Rectifier Distortion", "Hard Limiter 1413"]:
     pa.do('Import2: Filename="/home/gspusi/Music/aufnahmen/sultans_of_swing.mp3"')
     pa.do('SelectTime: Start="54.5" End="100"')
     pa.do('Delete')
@@ -11,6 +11,6 @@ for i in [5, 10, 20, 40, 80]:
     pa.do('SelAllTracks')
     pa.do('Normalize')
     pa.do('Compressor: Threshold=-20')
-    pa.do('Distortion: Threshold=-' + t)
-    pa.do('Export2: Filename="/home/gspusi/Music/aufnahmen/sultans_of_swing_pyaudacity_' + t + '.mp3"')
+    pa.do('Distortion: Threshold=-40 Type="' + s + '"')
+    pa.do('Export2: Filename="/home/gspusi/Music/aufnahmen/sultans_of_swing_pyaudacity_' + s + '.mp3"')
     pa.do('TrackClose')
