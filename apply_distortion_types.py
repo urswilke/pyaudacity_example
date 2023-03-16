cd7e7ff0d2b6e6fb3b5fbe1fe8b946884a55ede7
urswilke <urs.wilke@gmail.com>
2023-03-16 14:23:55 +0100

    import do

--- apply_distortion_types.py
+++ apply_distortion_types.py
@@ -1,5 +1,5 @@
 # start audacity from within the folder of this script before!
-import pyaudacity as pa
+from pyaudacity import do
 
 # put the name of your audio file in this folder:
 input_mp3 = "sultans_of_swing.mp3"
@@ -21,16 +21,16 @@ distortion_types = [
     "Hard Limiter 1413",
 ]
 for s in distortion_types:
-    pa.do('Import2: Filename="' + input_mp3 + '"')
-    pa.do('SelectTime: Start="54.5" End="100"')
-    pa.do('Delete')
-    pa.do('SelectTime: Start="0" End="2.6"')
-    pa.do('Delete')
-    pa.do('SelectTracks')
-    pa.do('SelectAll')
-    pa.do('SelAllTracks')
-    pa.do('Normalize')
-    pa.do('Compressor: Threshold=-20')
-    pa.do('Distortion: Threshold=-40 Type="' + s + '"')
-    pa.do('Export2: Filename="' + basename + '_pyaudacity_' + s + '.mp3"')
-    pa.do('TrackClose')
+    do('Import2: Filename="' + input_mp3 + '"')
+    do('SelectTime: Start="54.5" End="100"')
+    do('Delete')
+    do('SelectTime: Start="0" End="2.6"')
+    do('Delete')
+    do('SelectTracks')
+    do('SelectAll')
+    do('SelAllTracks')
+    do('Normalize')
+    do('Compressor: Threshold=-20')
+    do('Distortion: Threshold=-40 Type="' + s + '"')
+    do('Export2: Filename="' + basename + '_pyaudacity_' + s + '.mp3"')
+    do('TrackClose')
